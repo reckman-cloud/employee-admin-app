623da778e48ecccdc3505d70e9c1ea6a1e1f4d64
reckman-cloud <ryan.eckman@yahoo.com>
2025-12-23 14:19:18 -0800

    Add admin homepage with form link

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from '../screens/SignIn.jsx';
+import Home from '../screens/Home.jsx';
 import FormApp from '../FormApp.jsx';
 import RequireAdmin from '../security/RequireAdmin.jsx';
 
@@ -9,7 +10,8 @@ export default function AppRouter(){
     <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<SignIn/>} />
-        <Route path="/" element={<RequireAdmin><FormApp/></RequireAdmin>} />
+        <Route path="/" element={<RequireAdmin><Home/></RequireAdmin>} />
+        <Route path="/form" element={<RequireAdmin><FormApp/></RequireAdmin>} />
         <Route path="*" element={<Navigate to="/" replace/>} />
       </Routes>
     </BrowserRouter>
